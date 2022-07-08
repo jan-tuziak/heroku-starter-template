616448ee40ce4d35bff8ee609b9ea64752d3d52c
Jan Tuziak <jan.tuziak@gmail.com>
2022-07-08 14:22:44 +0000

    Working template

---
+++ main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+import _thread
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s :: %(module)s :: %(levelname)s :: %(message)s")
+import os
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return "hello world!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="127.0.0.1", port=port)
